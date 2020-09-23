3da428234bdc8255c4fb272d80a445c9c96bd8e4
davidsmorton <davidsmorton@gmail.com>
2020-09-23 13:32:43 +0000

    needed to add the array updated spinner1 code refactored

--- spinner2.js
+++ spinner2.js
@@ -1,5 +1,20 @@
-const spinner2 = require("./spinner1");
 
-console.log(spinner2);
 
 //also could have copied the logic from typewriter.js to refactor this. 
+
+
+const  spinner2 = function () {
+  const dash = ["\r|   ", '\r/   ', '\r-   ', '\r\\   ', '\r/   ', '\r-   ', '\r/   ', '\r|   ']
+  let timInt = 100
+  process.stdout.write("\n");
+  for(let i = 0; i < dash.length; i++) {
+    setTimeout(() => {
+        process.stdout.write(dash[i] );
+        }, timInt);
+    timInt += 200 
+  };
+}
+console.log(spinner2());
+
+
+// make an array used the logic from the typewriter. js file  
